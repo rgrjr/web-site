8b57cec52d336c9c417c0df42f46199d7d9476a3
Bob Rogers <rogers@rgrjr.com>
2003-08-06 19:26:18 +0000

    Implement approximate date matching in cvs-chrono-log.pl. Still no documentation or command-line parameterization.
    git-svn-id: https://rgr/svn/web-site/trunk@14 907b382b-8248-0410-9faa-85b0f4cab406

--- cvs-chrono-log.pl
+++ cvs-chrono-log.pl
@@ -12,6 +12,8 @@ use strict;
 use Date::Parse;
 use Date::Format;
 
+my $date_format_string = '%Y-%m-%d %H:%M';
+my $date_fuzz = 60;		# in seconds.
 my %modifications;
 
 sub record_file_rev_comment {
@@ -41,8 +43,10 @@ sub print_file_rev_comments {
     for my $comment (sort(keys(%modifications))) {
 	my $last_date;
 	my @entries;
-	for my $date (reverse(sort(keys(%{$modifications{$comment}})))) {
-	    if ($last_date) {
+	for my $date (sort(keys(%{$modifications{$comment}}))) {
+	    if ($last_date && $last_date-$date > $date_fuzz) {
+		# the current entry probably represents a "cvs commit" event
+		# that is distinct from the previous entry(ies).
 		push(@combined_entries, [$last_date, $comment, @entries]);
 		@entries = ();
 		undef($last_date);
@@ -54,16 +58,18 @@ sub print_file_rev_comments {
 	    if @entries;
     }
     # print Dumper(\%modifications, \@combined_entries);
-    # now print them out.
+    # now sort and print them out.
     for my $date_entry (sort { -($a->[0] <=> $b->[0]); } @combined_entries) {
-	my $date = time2str("%Y-%m-%d %H:%M", shift(@$date_entry));
+	my $date = time2str($date_format_string, shift(@$date_entry));
 	my $comment = shift(@$date_entry);
 	print "$date:\n";
 	for my $line (split("\n", $comment)) {
 	    print "  $line\n"
 		if $line;
 	}
-	for my $entry (@$date_entry) {
+	# CVS sorts the file names, but combining sets of entries with similar
+	# dates can make them come unsorted.
+	for my $entry (sort { $a->[0] cmp $b->[0]; } @$date_entry) {
 	    my ($file_name, $file_rev, $date_etc) = @$entry;
 	    print "  => $file_name $file_rev:  $date_etc\n";
 	}
