54d63a0bac46a6c943ff1464c0f67fd186403856
Bob Rogers <rogers@rgrjr.com>
2004-05-09 01:37:03 +0000

    Now generates HTML output, with hyperlinks to files (if present in certain hard-wired directories).  The standard input is a template; if a line with:
    	<!-- insert-log -->
    
    is seen in the input, it is replaced by the HTML-ized CVS log output.
    
    
    git-svn-id: https://rgr/svn/web-site/trunk@47 907b382b-8248-0410-9faa-85b0f4cab406

--- cvs-chrono-log.pl
+++ cvs-chrono-log.pl
@@ -10,10 +10,17 @@
 use strict;
 use Date::Parse;
 use Date::Format;
+use CGI qw(escapeHTML);
 
 my $date_format_string = '%Y-%m-%d %H:%M';
 my $date_fuzz = 60;		# in seconds.
 my %modifications;
+
+my $html_p = 1;
+my $n_days_ago = 90;
+my $start_date = time2str($date_format_string, time-24*3600*$n_days_ago);
+my $working_directory = '/home/rogers/projects/web-site';
+my $web_root = '/usr/local/aolserver/servers/rgrjr/pages';
 
 sub record_file_rev_comment {
     my ($file_name, $file_rev, $date_etc, $comment) = @_;
@@ -61,7 +68,7 @@ sub print_file_rev_comments {
 	my $date = time2str($date_format_string, shift(@$date_entry));
 	my $comment = shift(@$date_entry);
 	print "$date:\n";
-	for my $line (split("\n", $comment)) {
+	for my $line (split("\n", escapeHTML($comment))) {
 	    print "  $line\n"
 		if $line;
 	}
@@ -69,7 +76,11 @@ sub print_file_rev_comments {
 	# dates can make them come unsorted.
 	for my $entry (sort { $a->[0] cmp $b->[0]; } @$date_entry) {
 	    my ($file_name, $file_rev, $date_etc) = @$entry;
-	    print "  => $file_name $file_rev:  $date_etc\n";
+	    my $pretty_file_name
+		= (-r "$web_root/$file_name"
+		   ? "<a href='/$file_name'><tt>/$file_name</tt></a>"
+		   : "<tt>$file_name</tt>");
+	    print "  => $pretty_file_name $file_rev:  $date_etc\n";
 	}
 	print "\n";
     }
@@ -77,48 +88,67 @@ sub print_file_rev_comments {
 
 ### Parser top level.
 
-# state is one of qw(none headings descriptions).
-my $state = 'none';
-my $file_name;
-my $line;
-while (defined($line = <>)) {
-    if ($line =~ /^RCS file: /) {
-	# start of a new entry.
-	$state = 'headings';
-    }
-    elsif ($state eq 'descriptions') {
-	# print($line);
-	$line =~ /^revision (.*)$/
-	    or warn "[oops; expected revision here.]\n";
-	my $file_rev = $1;
-	chomp(my $date_etc = <>);
-	my $comment = '';
-	$line = <>;
-	if ($line =~ /^branches: /) {
-	    chomp($line);
-	    $date_etc .= "  ".$line;
-	    $line = <>;
+sub cvs_log_to_html_log {
+    print "<pre>\n";
+    # state is one of qw(none headings descriptions).
+    my $state = 'none';
+    my $file_name;
+    my $line;
+    open(IN, "cd '$working_directory'; cvs -q log -d '>$start_date' |")
+	or die "oops; couldn't open pipe from cvs:  $!";
+    while (defined($line = <IN>)) {
+	if ($line =~ /^RCS file: /) {
+	    # start of a new entry.
+	    $state = 'headings';
 	}
-	while ($line !~ /^---+$|^===+$/) {
-	    $comment .= $line;
-	    $line = <>;
-	}
-	record_file_rev_comment($file_name, $file_rev, $date_etc, $comment);
-	$state = ($line =~ /^========/ ? 'none' : 'descriptions');
-    }
-    # $state eq 'headings'
-    elsif ($line =~ /^([^:]+):\s*(.*)/) {
-	# processing the file header.
-	my $tag = $1;
-	if ($tag eq 'description') {
-	    $line = <>;
+	elsif ($state eq 'descriptions') {
+	    # print($line);
+	    $line =~ /^revision (.*)$/
+		or warn "[oops; expected revision here.]\n";
+	    my $file_rev = $1;
+	    chomp(my $date_etc = <IN>);
+	    my $comment = '';
+	    $line = <IN>;
+	    if ($line =~ /^branches: /) {
+		chomp($line);
+		$date_etc .= "  ".$line;
+		$line = <IN>;
+	    }
+	    while ($line !~ /^---+$|^===+$/) {
+		$comment .= $line;
+		$line = <IN>;
+	    }
+	    record_file_rev_comment($file_name, $file_rev, $date_etc, $comment);
 	    $state = ($line =~ /^========/ ? 'none' : 'descriptions');
 	}
-	elsif ($tag eq 'Working file') {
-	    chomp($file_name = $2);
+	# $state eq 'headings'
+	elsif ($line =~ /^([^:]+):\s*(.*)/) {
+	    # processing the file header.
+	    my $tag = $1;
+	    if ($tag eq 'description') {
+		$line = <IN>;
+		$state = ($line =~ /^========/ ? 'none' : 'descriptions');
+	    }
+	    elsif ($tag eq 'Working file') {
+		chomp($file_name = $2);
+	    }
 	}
     }
+    close(IN);
+    warn "[oops; final state is $state.]\n"
+	unless $state eq 'none';
+    print_file_rev_comments();
+
+    print "</pre>\n\n";
 }
-warn "[oops; final state is $state.]\n"
-    unless $state eq 'none';
-print_file_rev_comments();
+
+### Main loop.
+
+while (<>) {
+    if ($_ =~ /^\s*<!--\s+insert-log\s*-->\s*$/) {
+	cvs_log_to_html_log();
+    }
+    else {
+	print;
+    }
+}
