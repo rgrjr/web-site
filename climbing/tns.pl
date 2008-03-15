5e4d615142fe26cffc4acb16b208c3650993d061
Bob Rogers <rogers@rgrjr.com>
2008-03-15 22:38:39 +0000

    * tns.pl:    + Use Getopt::Long so we can parse more options (still no doc).    + Support --locations, --start, --end, and --holidays options.    + Add explicit validation of location abbrevs.    + Replace dates file with enumeration between --start and --end, with      suitable adjustment for holidays on Monday or Tuesday.    + Don't require the dates for --summary output.    + Die properly if # of dates & # of locations don't agree. * makefile:    + (sched-2008.html):  New.    + (test-tns-1):  Use the 2008 sites to test the new tns.pl options. * test-tns-1.html (added):    + Test data for the test-tns-1 case.
    git-svn-id: https://rgr/svn/web-site/trunk@155 907b382b-8248-0410-9faa-85b0f4cab406

--- climbing/tns.pl
+++ climbing/tns.pl
@@ -15,28 +15,41 @@
 #
 
 use strict;
+use warnings;
+
+use Date::Parse;
+use Date::Format;
+use Getopt::Long;
 
 my $summary_only_p = 0;
-# my $tuesday_night_home = '/usr/local/aolserver/servers/rgrjr/pages/climbing/';
-my $tuesday_night_home = '.';
-
+my $locations_file = "locations.tbl";
+# These have to be specified as command-line options.
+my ($start_time, $end_time, @holidays);
 # Parse any args
-while ($ARGV[0] =~ /^-./) {
-    my $arg = shift(@ARGV);
-    if ($arg eq '-summary') {
-	$summary_only_p = $arg;
-    }
-    else {
-	die "tns.pl:  Unknown argument '$arg'.\nDied";
-    }
-}
+GetOptions('summary!' => \$summary_only_p,
+	   'locations=s' => \$locations_file,
+	   'start=s' => sub { my ($arg, $start_date) = @_;
+			      $start_time = str2time($start_date)
+				  or die "Bad start date";
+	   },
+	   'end=s' => sub { my ($arg, $end_date) = @_;
+			    $end_time = str2time($end_date)
+				or die "Bad end date";
+	   },
+	   'holidays=s' 
+	       => sub { my ($arg, $holiday) = @_;
+			push(@holidays,
+			     map { str2time($_) || die "Bad data '$_'";
+			     } split(/,/, $holiday));
+	   });
+
 my @tuesday_locations = @ARGV;
 my %tn_name;
 my %tn_url;
 my %tn_description;
 
 # First, load locations.tbl.
-open(IN, "$tuesday_night_home/locations.tbl") || die;
+open(IN, $locations_file) || die;
 my $line;
 while (defined($line = <IN>)) {
     chomp($line);
@@ -46,38 +59,78 @@ while (defined($line = <IN>)) {
     $tn_description{$abbrev} = $description;
 }
 
-# Read the dates.
-open(IN, "$tuesday_night_home/dates.tbl") || die;
-chomp(my @dates = <IN>);
-
-if (@dates != @tuesday_locations) {
-    warn (sprintf("tns.pl:  %d locations for %d dates.\n",
-		  scalar @tuesday_locations, scalar @dates));
+# Validate abbreviations.
+my $n_errors = 0;
+for my $abbrev (@tuesday_locations) {
+    if (! $tn_name{$abbrev}) {
+	warn("'$abbrev' is not one of '",
+	     join("', '", sort(keys(%tn_name))), "'.\n");
+	$n_errors++;
+    }
 }
+exit(1)
+    if $n_errors;
 
+# If requested, summarize counts by location, and exit.
 if ($summary_only_p) {
-    # Summarize counts by location.
     my %count;
-    foreach my $abbrev (@tuesday_locations) {
+    for my $abbrev (@tuesday_locations) {
 	$count{$abbrev}++;
     }
-    foreach my $abbrev (sort(keys(%count))) {
+    for my $abbrev (sort(keys(%count))) {
 	printf("%7d\t%s\n", $count{$abbrev}, $tn_name{$abbrev});
     }
+    exit(0);
 }
-else {
-    # Generate the table rows using locations specified on the command line.
-    my $i = 0;
-    foreach my $abbrev (@tuesday_locations) {
-	my $date = $dates[$i++];
-	print "  <tr>\n    <td>$date</td>\n";
-	print "    <td>$tn_name{$abbrev}</td>\n";
-	my $url = $tn_url{$abbrev};
-	print("    <td>",
-	      ($url ? "<a href='$url'>" : ''),
-	      $tn_description{$abbrev},
-	      ($url ? "</a>" : ''),
-	      "</td>\n");
-	print "  </tr>\n";
+
+# Find out whether we're too close to a holiday.
+my $secs_per_day = 24*60*60;
+my $secs_per_week = 7*$secs_per_day;
+sub after_holiday_p {
+    # If a holiday falls on a Monday or Tuesday of the same week, then climbing
+    # should happen on Wednesday.
+    my $time = shift;
+
+    for my $holiday (@holidays) {
+	my $interval = $time-$holiday;
+	return 1
+	    if $interval > 0 && $interval < 2*$secs_per_day;
     }
+    return 0;
+}
+
+# Figure out the dates.
+die "$0:  Must specify --start and --end dates.\n"
+    unless $start_time && $end_time;
+my $time = $start_time;
+my @dates;
+while ($time <= $end_time) {
+    my $day;
+    if (after_holiday_p($time)) {
+	$day = time2str('%d %b (WED)', $time+$secs_per_day);
+    }
+    else {
+	$day = time2str('%d %b', $time);
+    }
+    # warn "time $time => day $day\n";
+    push(@dates, $day);
+    $time += $secs_per_week;
+}
+die('tns.pl:  ', scalar(@tuesday_locations), ' locations for ',
+    scalar(@dates), " dates.\n")
+    if @dates != @tuesday_locations;
+
+# Generate the table rows using locations specified on the command line.
+my $i = 0;
+foreach my $abbrev (@tuesday_locations) {
+    my $date = $dates[$i++];
+    print "  <tr>\n    <td>$date</td>\n";
+    print "    <td>$tn_name{$abbrev}</td>\n";
+    my $url = $tn_url{$abbrev};
+    print("    <td>",
+	  ($url ? "<a href='$url'>" : ''),
+	  $tn_description{$abbrev},
+	  ($url ? "</a>" : ''),
+	  "</td>\n");
+    print "  </tr>\n";
 }
